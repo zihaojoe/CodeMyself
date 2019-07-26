070b5fc3b1f7f64e1829427454d8191accb0831c
Joe Zhang <zihaojoe@163.com>
2019-07-26 21:29:55 +0800

    Updated

--- R/EDA.R
+++ R/EDA.R
@@ -2,7 +2,8 @@
 # 安装依赖库
 install.packages('ggplot2', dependencies = TRUE)
 install.packages('ggthemes', dependencies = TRUE)
-## 参考文档 
+install.packages('gridExtra', dependencies = TRUE)   # 一个画布上显示
+## ggplot参考文档 
 ## https://www.jianshu.com/p/0c25e1904026
 
 # ----------------------------------------------------
@@ -24,6 +25,7 @@ qplot(data=data, x=age.range)
 # lab2:Facebook虚假数据统计分析
 setwd('C:/Users/01/Downloads')
 library(ggthemes)
+library(ggplot2)
 pf = read.csv('pseudo_facebook.tsv', sep='\t')
 
 ## 创建图并修改x轴的值
@@ -44,5 +46,23 @@ qplot(data=pf, x=friend_count, binwidth=25) + scale_x_continuous(limits=c(0, 100
 table(pf$gender)
 by(pf$friend_count, pf$gender, summary)   #第一个参数是变量，第二个是分类依据，第三个是返回描述
 
+## 一张画布多张图
+library(gridExtra)
+### 方法一：
+p1 <- qplot(data=pf, x=friend_count)
+p2 <- qplot(data=pf, x=log10(friend_count+1))
+p3 <- qplot(data=pf, x=sqrt(friend_count+1))
+grid.arrange(p1, p2, p3, ncol=1)
+### 方法二：
+rm(p1);rm(p2);rm(p3)
+p1 <- ggplot(data=pf, aes(x=friend_count)) + ggeom_histogram()
+p2 <- p1 + scale_x_log10()
+p3 <- p1 + scale_x_sqrt()
+grid.arrange(p1, p2, p3, ncol=1)
 
+## 频数多边形(frequency polygon)
+qplot(x = friend_count, y = ..count../sum(..count..), 
+      data = subset(pf, !is.na(gender)), binwidth=10,
+      geom="freqpoly", color=gender) +
+  scale_x_continuous(limits = c(0, 1000), breaks = seq(0, 1000, 50))
 
