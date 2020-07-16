ddf790d29b3ee457877f1674c99372095c043ca9
zihaojoe <43315069+zihaojoe@users.noreply.github.com>
2020-07-15 19:23:33 -0500

    Update log.py

--- Python_Useful/utility_module/log/log.py
+++ Python_Useful/utility_module/log/log.py
@@ -4,9 +4,15 @@ import logging
 import logging.config
 
 #logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# disable logger from certain packages, like fbprophet or pandas
+logger = logging.getLogger("fbprophet")
+logger.setLevel(logging.WARNING)
+
+# root logger of the main function
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
 logger.info("Start print log")
 logger.debug("Do something")
-logger.warning("Something maybe fail.")
+logger.warning("Something maybe fail")
 logger.info("Finish")
